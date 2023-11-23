ce709426057191531b04e63ffd91172a4f3ec629
jordanelizarde2@gmail.com <jordanelizarde2@gmail.com>
2023-11-23 16:28:25 +0000

    Created ITEMS and MYCART; Edited HOME and BASE

--- client_code/BASE/__init__.py
+++ client_code/BASE/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import BASETemplate
 from anvil import *
 from ..HOME import HOME
+
 class BASE(BASETemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -8,3 +9,8 @@ class BASE(BASETemplate):
     self.content_panel.add_component(HOME())
 
     # Any code you write here will run before the form opens.
+
+  def title_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(HOME())
--- client_code/HOME/__init__.py
+++ client_code/HOME/__init__.py
@@ -1,10 +1,16 @@
 from ._anvil_designer import HOMETemplate
 from anvil import *
-
+from ..ITEMS import ITEMS
 
 class HOME(HOMETemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
 
     # Any code you write here will run before the form opens.
+
+  def items_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(ITEMS())
