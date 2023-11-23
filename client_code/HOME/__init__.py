bbe6eaa61b2c8a141fccd1b46c47d5247d74a60e
jordanelizarde2@gmail.com <jordanelizarde2@gmail.com>
2023-11-23 14:44:26 +0000

    Created HOME, output-onlinegiftools.gif and animated_tanuki.gif; Edited BASE and theme/parameters.yaml

--- client_code/HOME/__init__.py
+++ client_code/HOME/__init__.py
@@ -1,10 +1,10 @@
-from ._anvil_designer import BASETemplate
+from ._anvil_designer import HOMETemplate
 from anvil import *
-from ..HOME import HOME
-class BASE(BASETemplate):
+
+
+class HOME(HOMETemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(HOME())
 
     # Any code you write here will run before the form opens.
